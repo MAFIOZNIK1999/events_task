75f7ac8fb8a38fd1043893fe29be91ec552433ce
User <user@user.com>
2025-02-21 13:13:38 +0200

    flake8 and added const

--- events/filters.py
+++ events/filters.py
@@ -5,7 +5,7 @@ from events.models import Event
 
 class EventFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr="icontains")
-    date = django_filters.DateFilter()
+    date = django_filters.DateTimeFilter()
     location = django_filters.CharFilter(lookup_expr="icontains")
     organizer = django_filters.CharFilter(lookup_expr="icontains")
 
--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+MIN_LENGTH_PASSWORD = 5
+
 User = get_user_model()
 
 
@@ -9,7 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "username", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {"password": {"write_only": True, "min_length": MIN_LENGTH_PASSWORD}}
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
